523543539a1f4500392b3837fc0dd6635121f279
eriejar <michael16574@gmail.com>
2018-10-30 22:02:47 -0700

    commit full

---
+++ UnitTest/Utilities/LastFMUtilitiesTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicLog.LastFM;
+
+namespace MusicLog.Tests.Utilities
+{
+    [TestClass]
+    public class LastFMUtilitiesTests
+    {
+        [TestMethod]
+        public void GetUserTracksTest()
+        {
+            List<LastFMAPIClient.Track> tracks = LastFMApi.GetUserTracks("eriejar", "Caravan Palace");
+            Assert.IsNotNull(tracks[0]);
+        }
+
+    }
+}
